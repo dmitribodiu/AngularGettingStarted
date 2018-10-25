11536f9a21fde27e1729c68f41b6c3de751999fb
DmitriBodiu <dmitri.bodiu@katoennatie.com>
2018-10-25 14:58:03 +0300

    Chapter 7

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./products";
+import { runInThisContext } from "vm";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+    
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 
     constructor() {
     }
@@ -13,31 +20,46 @@ export class ProductListComponent{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
-    products: any[] = [
+    
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.perFormFilter(this.listFilter): this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
-            "productId": 2,
-            "productName": "GArden Cart",
-            "productCode": "GDN-002",
-            "releaseDate": "March 18, 2018",
-            "description": "15 gallon capacity rolling garden",
-            "price": 23.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/sv"
+            productId: 1,
+            productName: "GArden Cart",
+            productCode: "GDN-002",
+            releaseDate: "March 18, 2018",
+            description: "15 gallon capacity rolling garden",
+            price: 23.99,
+            starRating: 4.2,
+            imageUrl: "https://openclipart.org/image/300px/sv"
         },
         {
-            "productId": 2,
-            "productName": "GArden Cart",
-            "productCode": "GDN-002",
-            "releaseDate": "March 18, 2018",
-            "description": "15 gallon capacity rolling garden",
-            "price": 23.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/sv"
+            productId: 2,
+            productName: "GArden Cart",
+            productCode: "GDN-002",
+            releaseDate: "March 18, 2018",
+            description: "15 gallon capacity rolling garden",
+            price: 23.99,
+            starRating: 4.2,
+            imageUrl: "https://openclipart.org/image/300px/sv"
         }   
     ];
 
     toggleImage(value: string): void {
         this.showImage = !this.showImage;
+    }
+
+    perFormFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 }
