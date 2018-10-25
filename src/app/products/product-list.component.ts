4ae0133d4be75438de9a180a4353b2f8a7357f09
DmitriBodiu <dmitri.bodiu@katoennatie.com>
2018-10-25 12:28:08 +0300

    Data Binding & Pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -10,7 +10,34 @@ export class ProductListComponent{
     constructor() {
     }
     pageTitle: string = 'Product List';
-    products: any[] = [{
-        
-    }];
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = "cart";
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "GArden Cart",
+            "productCode": "GDN-002",
+            "releaseDate": "March 18, 2018",
+            "description": "15 gallon capacity rolling garden",
+            "price": 23.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/sv"
+        },
+        {
+            "productId": 2,
+            "productName": "GArden Cart",
+            "productCode": "GDN-002",
+            "releaseDate": "March 18, 2018",
+            "description": "15 gallon capacity rolling garden",
+            "price": 23.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/sv"
+        }   
+    ];
+
+    toggleImage(value: string): void {
+        this.showImage = !this.showImage;
+    }
 }
